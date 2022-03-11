c7508957fc28ea0a0fbc09c38b79dcf02d43e530
Abolfazl Abdi <dev.abolfazlabdi@gmail.com>
2022-03-11 14:15:09 +0330

    Add: delete user avatar signal

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -12,7 +12,7 @@ class User(AbstractUser):
 
     bio = models.TextField(default='بیوگرافی خودرا ویرایش کنید', verbose_name='بیوگرافی')
     avatar = models.ImageField(
-        upload_to=upload_path('accounts/avatars'), default='default.jpg', validators=[validate_avatar], verbose_name='avatar'
+        upload_to=upload_path('accounts/avatars'), default='accounts/avatars/default.jpg', validators=[validate_avatar], verbose_name='avatar'
     )
     is_employee = models.BooleanField(default=False, verbose_name='کارمند')
     is_customer = models.BooleanField(default=False, verbose_name='کارفرما')
