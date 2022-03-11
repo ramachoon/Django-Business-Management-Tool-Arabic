40ce88062eb60649ae3c58d1161c515f390b4103
Abolfazl Abdi <dev.abolfazlabdi@gmail.com>
2022-03-11 12:56:44 +0330

    Customize user model

---
+++ src/accounts/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
+# Register your models here.
+
+UserAdmin.fieldsets[1][1]['fields'] = ('first_name', 'last_name', 'email', 'avatar', 'bio')
+UserAdmin.fieldsets[2][1]['fields'] = (
+    'is_active', 'is_staff', 'is_superuser', 'is_employee', 'is_customer', 'is_admin', 'is_supporter',
+    'groups', 'user_permissions'
+)
+
+admin.site.register(User, UserAdmin)
