3b46153a6fd37e24d39dcda01feea509dc3276bc
Abolfazl Abdi <dev.abolfazlabdi@gmail.com>
2022-03-13 19:31:23 +0330

    Add: verify_phone_otp view

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import (
-    otp_login
+    otp_login,
+    verify_phone_otp
 )
 
 app_name = 'account'
 urlpatterns = [
-    path('login/', otp_login, name='login')
+    path('login/', otp_login, name='login'),
+    path('login/verify/', verify_phone_otp, name='verify_otp'),
 ]
--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,6 +1,15 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-from .forms import LoginForm
+import random
+from hashlib import sha256
+
+from django.contrib.auth import login
+from django.http import Http404
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.utils import timezone
+from django.contrib import messages
+
+from .forms import LoginForm, VerifyOtpForm
+from accounts.models import PhoneOtp, User
 
 
 # Create your views here.
@@ -11,9 +20,55 @@ def otp_login(request):
 
     if login_form.is_valid():
         phone_number = login_form.cleaned_data.get('phone_number')
-        return HttpResponse('ok')
+        _code = random.randint(111111, 999999)
+        print(_code)
+        hash_code = sha256(str(_code).encode('utf-8')).hexdigest()
+
+        request.session['phone_number'] = phone_number
+
+        try:
+            phone_otp = PhoneOtp.objects.get(phone=phone_number)
+            phone_otp.code = hash_code
+            phone_otp.save()
+        except PhoneOtp.DoesNotExist:
+            phone_otp = PhoneOtp(phone=phone_number, code=hash_code)
+            phone_otp.save()
+
+        return redirect(reverse('account:verify_otp'))
 
     context = {
         'form': login_form
     }
     return render(request, 'accounts/registration/login.html', context)
+
+
+def verify_phone_otp(request):
+    # raise http404, if http referer is not equal to login.
+    if not request.META.get('HTTP_REFERER') == f"{request.scheme}://{request.get_host()}{reverse('account:login')}":
+        raise Http404
+
+    verify_otp_form = VerifyOtpForm(request.POST or None)
+    phone_number = request.session['phone_number']
+
+    if verify_otp_form.is_valid():
+        _code = verify_otp_form.cleaned_data.get('code')
+        hash_code = sha256(str(_code).encode('utf-8')).hexdigest()
+        phone_otp = PhoneOtp.objects.filter(phone=phone_number, code=hash_code)
+        # check phone_otp is exist and check the past moments.
+        if phone_otp.exists() and \
+                timezone.now().minute - phone_otp.first().updated.minute <= 5:
+            # get user and authenticate
+            try:
+                user = User.objects.get(username=phone_number)
+                login(request, user=user)
+                del request.session['phone_number']
+            except User.DoesNotExist:
+                raise Http404
+        else:
+            messages.error(request, 'کد تائید اشتباه وارد شده است.')
+
+    context = {
+        'form': verify_otp_form,
+        'phone_number': phone_number
+    }
+    return render(request, 'accounts/registration/verify_otp.html', context)
