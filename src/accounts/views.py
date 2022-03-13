c89e332df6c3b73226783c989524cdb22b4109f2
Abolfazl Abdi <dev.abolfazlabdi@gmail.com>
2022-03-13 15:54:38 +0330

    Add: otp_login view

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,3 +1,19 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .forms import LoginForm
+
 
 # Create your views here.
+
+
+def login_view(request):
+    login_form = LoginForm(request.POST or None)
+
+    if login_form.is_valid():
+        phone_number = login_form.cleaned_data.get('phone_number')
+        return HttpResponse('ok')
+
+    context = {
+        'form': login_form
+    }
+    return render(request, 'accounts/registration/login.html', context)
