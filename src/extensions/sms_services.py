67beadf490cba40ebc473444087d9dd0ba5cd3cd
Ramachandran <ramachoon@gmail.com>
2022-03-13 20:27:35 +0330

    Update: sms_services

--- src/extensions/sms_services.py
+++ src/extensions/sms_services.py
@@ -3,11 +3,8 @@ import json
 import requests
 from django.conf import settings
 
-username = settings.SMS_PANEL_USERNAME
-password = settings.SMS_PANEL_PASSWORD
+api_address = settings.SMS_PANEL_SAMPLE_API
 phone_number = settings.SMS_PANEL_PHONE_NUMBER
-send_sms_url = 'https://rest.payamak-panel.com/api/SendSMS/SendSMS'
-deliveries_sms_url = 'https://rest.payamak-panel.com/api/SendSMS/GetDeliveries2'
 request_header = {
     'accept': 'application/json',
     'Content-Type': 'application/json'
@@ -16,18 +13,12 @@ request_header = {
 
 def send_otp_sms(to, code):
     send_sms_data = {
-        'username': username,
-        'password': password,
         'to': to,
         'from': phone_number,
         'text': f'کدتائید: {code} \nگروه فلزکاری برادران عبدی'
     }
     send_sms_req = requests.post(
-        url=send_sms_url, data=json.dumps(send_sms_data), headers=request_header
+        url=api_address, data=json.dumps(send_sms_data), headers=request_header
     )
-    ret_status = send_sms_req.json()['RetStatus']
-    str_ret_status = send_sms_req.json()['StrRetStatus']
-    if ret_status == 1 and str_ret_status == "Ok":
+    if send_sms_req.json()['status'] == 'ارسال موفق بود':
         return True
-    else:
-        return False
