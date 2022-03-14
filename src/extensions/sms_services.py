a6a472086bbda539a4b3484700bea83860837dc1
Abolfazl Abdi <dev.abolfazlabdi@gmail.com>
2022-03-14 16:54:33 +0330

    Update: sms services

--- src/extensions/sms_services.py
+++ src/extensions/sms_services.py
@@ -6,16 +6,15 @@ from django.conf import settings
 api_address = settings.SMS_PANEL_SAMPLE_API
 phone_number = settings.SMS_PANEL_PHONE_NUMBER
 request_header = {
-    'accept': 'application/json',
     'Content-Type': 'application/json'
 }
 
 
 def send_otp_sms(to, code):
     send_sms_data = {
+        'bodyId': 79780,
         'to': to,
-        'from': phone_number,
-        'text': f'کدتائید: {code} \nگروه فلزکاری برادران عبدی'
+        'args': [f"{code}"]
     }
     send_sms_req = requests.post(
         url=api_address, data=json.dumps(send_sms_data), headers=request_header
