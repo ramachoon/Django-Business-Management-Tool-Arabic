62947e3e464c4eea356d80a6e16f8791f1608d14
Ramachandran <ramachoon@gmail.com>
2022-03-16 19:01:58 +0330

    Add: SuperuserAccess mixin

--- src/core/views.py
+++ src/core/views.py
@@ -1,8 +1,13 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
-
+from django.shortcuts import redirect
+from django.urls import reverse
 
 # Create your views here.
+
+
 @login_required(login_url='account:login')
 def main_view(request):
+    if request.user.is_superuser:
+        return redirect(reverse('managers:user_list'))
     return HttpResponse('خوش آمدید')
