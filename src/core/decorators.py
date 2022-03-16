304505b2d78678651add6d214f33567f5def3cb7
Abolfazl Abdi <dev.abolfazlabdi@gmail.com>
2022-03-16 23:44:04 +0330

    Add: user activate deactivate view and ajax

--- src/core/decorators.py
+++ src/core/decorators.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import redirect
 from django.urls import reverse
 
@@ -12,6 +13,18 @@ def authenticated_user():
             if request.user.is_authenticated:
                 return redirect(reverse('core:main_view'))
             return view_func(request)
+
+        return inner
+
+    return decorator
+
+
+def superuser_access_decorator():
+    def decorator(view_func):
+        def inner(request, *args, **kwargs):
+            if request.user.is_authenticated and request.user.is_superuser:
+                return view_func(request, *args, **kwargs)
+            return Http404
 
         return inner
 
--- src/managers/urls.py
+++ src/managers/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('users/create/', managers_views.UserCreate.as_view(), name='user_create'),
     path('users/update/<int:pk>', managers_views.UserUpdate.as_view(), name='user_update'),
     path('users/delete/<int:pk>', managers_views.UserDelete.as_view(), name='user_delete'),
+    path('users/is-active', managers_views.user_activate_deactivate, name='user_is_active'),
 ]
