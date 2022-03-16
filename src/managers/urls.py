789f280565987e9f1a0b692c3f989e5090e2ecfa
Abolfazl Abdi <dev.abolfazlabdi@gmail.com>
2022-03-16 18:33:47 +0330

    Add: UserCreate view

--- src/managers/urls.py
+++ src/managers/urls.py
@@ -4,4 +4,5 @@ from . import views as managers_views
 app_name = 'managers'
 urlpatterns = [
     path('users/', managers_views.UsersList.as_view(), name='user_list'),
+    path('users/create/', managers_views.UserCreate.as_view(), name='user_create'),
 ]
