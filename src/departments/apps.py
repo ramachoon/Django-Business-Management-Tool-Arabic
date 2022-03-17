190d4960a522e41b36b670132c6e240d846d9660
Ramachandran <ramachoon@gmail.com>
2022-03-17 12:38:58 +0330

    startapp departments & Add: Department model

---
+++ src/departments/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class DepartmentsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'departments'
+    verbose_name = '2. دپارتمان ها'
---
+++ src/departments/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from accounts.models import User
+
+
+# Create your models here.
+class Department(models.Model):
+    name = models.CharField(max_length=50, verbose_name='نام دپارتمان')
+    staff_users = models.ManyToManyField(
+        User, related_name='departments', blank=True, verbose_name='کاربران ارشد'
+    )
+    description = models.TextField(verbose_name='توضیحات دپارتمان')
+    maker = models.ForeignKey(
+        User, related_name='departments_owner', on_delete=models.CASCADE, verbose_name='سازنده'
+    )
+    is_active = models.BooleanField(default=True, verbose_name='فعال')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='ایجاد')
+    updated = models.DateTimeField(auto_now=True, verbose_name='آخرین ویرایش')
+
+    class Meta:
+        verbose_name = 'دپارتمان'
+        verbose_name_plural = 'دپارتمان ها'
+
+    def __str__(self):
+        return f"{self.name} | {self.description}"
+
+    def get_name_replace(self):
+        return f"{self.name.replace(' ', '-')}"
