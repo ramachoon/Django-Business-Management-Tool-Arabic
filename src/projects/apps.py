5f8283f9b7eb1fb691c8788d886dca3973b3471a
Abolfazl Abdi <dev.abolfazlabdi@gmail.com>
2022-03-17 15:52:39 +0330

    startapp projects & Add: Project model

---
+++ src/projects/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class ProjectsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'projects'
+    verbose_name = '3. پروژه ها'
