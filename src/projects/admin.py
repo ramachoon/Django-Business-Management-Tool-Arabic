f540691deaee26da3a87df6146531152c2e265a4
Abolfazl Abdi <dev.abolfazlabdi@gmail.com>
2022-03-19 17:11:59 +0330

    Add: Invoice & InvoiceDetail model

--- src/projects/admin.py
+++ src/projects/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Project, WorkDay
+from .models import Project, WorkDay, Invoice, InvoiceDetail
 
 # Register your models here.
 admin.site.register(Project)
 admin.site.register(WorkDay)
+admin.site.register(Invoice)
+admin.site.register(InvoiceDetail)
