1516b0702fd173df7cfa6077654ac83b082068ca
Abolfazl Abdi <dev.abolfazlabdi@gmail.com>
2022-03-19 19:16:54 +0330

    Add: pwa

--- src/config/urls.py
+++ src/config/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('src.core.urls')),
+    path('', include('pwa.urls')),
     path('account/', include('src.accounts.urls')),
     path('manager/', include('src.managers.urls')),
     path('admin/', admin.site.urls),
