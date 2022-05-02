ded9481a79e24b5233cc09d2e6dbccf965967b9f
Ramachandran <ramachoon@gmail.com>
2022-05-02 18:22:29 +0430

    Add: StaffDepartmentList & StaffDepartmentDetail

--- src/staff_module/urls.py
+++ src/staff_module/urls.py
@@ -3,5 +3,9 @@ from . import views as staff_views
 
 app_name = 'staff_module'
 urlpatterns = [
-    path('', staff_views.staff_home_page, name='home')
+    path('', staff_views.staff_home_page, name='home'),
+
+    # departments
+    path('departments/', staff_views.StaffDepartmentList.as_view(), name='department_list'),
+    path('departments/<int:pk>', staff_views.StaffDepartmentDetail.as_view(), name='department_detail'),
 ]
