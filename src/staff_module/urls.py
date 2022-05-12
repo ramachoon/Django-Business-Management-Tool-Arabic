4da0f60cb2e92c0e7b38cc4a7f42826530b6fc52
Ramachandran <ramachoon@gmail.com>
2022-05-12 14:43:24 +0430

    Add: StaffProjectList & StaffProjectDetail

--- src/staff_module/urls.py
+++ src/staff_module/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
     # departments
     path('departments/', staff_views.StaffDepartmentList.as_view(), name='department_list'),
     path('departments/<int:pk>', staff_views.StaffDepartmentDetail.as_view(), name='department_detail'),
+
+    # projects
+    path('projects/', staff_views.StaffProjectList.as_view(), name='project_list'),
+    path('projects/<int:pk>', staff_views.StaffProjectDetail.as_view(), name='project_detail'),
 ]
--- src/staff_module/views.py
+++ src/staff_module/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView
 from departments.models import Department
+from projects.models import Project
 
 
 # Create your views here.
+
+
 def staff_home_page(request):
     return render(request, 'staff/staff_home_page.html')
 
@@ -28,3 +31,27 @@ class StaffDepartmentDetail(DetailView):
 
     template_name = 'staff/department_detail.html'
     context_object_name = 'department'
+
+
+class StaffProjectList(ListView):
+    def get_queryset(self):
+        qs = Project.objects.filter(
+            department__staff_users__in=[self.request.user], department__is_active=True
+        )
+        return qs
+
+    template_name = 'staff/project_list.html'
+    paginate_by = 6
+    context_object_name = 'projects'
+
+
+class StaffProjectDetail(DetailView):
+    def get_object(self, queryset=None):
+        pk = self.kwargs.get('pk')
+        qs = get_object_or_404(
+            Project, pk=pk, department__staff_users__in=[self.request.user], department__is_active=True
+        )
+        return qs
+
+    template_name = 'staff/project_detail.html'
+    context_object_name = 'project'
