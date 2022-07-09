b903a88b9fce94af61e9e9dd93cbd53d1b6e6823
Abolfazl Abdi <dev.abolfazlabdi@gmail.com>
2022-07-09 18:30:54 +0430

    Add: CustomerProjectList & CustomerProjectDetail views

--- src/customer_module/urls.py
+++ src/customer_module/urls.py
@@ -4,5 +4,9 @@ from . import views as customer_views
 app_name = 'customer_module'
 
 urlpatterns = [
-    path('', customer_views.customer_home_page, name='home')
+    path('', customer_views.customer_home_page, name='home'),
+
+    # projects
+    path('projects/', customer_views.CustomerProjectList.as_view(), name='project_list'),
+    path('projects/<int:pk>', customer_views.CustomerProjectDetail.as_view(), name='project_detail'),
 ]
--- src/customer_module/views.py
+++ src/customer_module/views.py
@@ -1,5 +1,10 @@
+from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+
 from core.decorators import customer_access_decorator
+from core.mixins import CustomerAccessMixin
+from projects.models import Project
 
 
 # Create your views here.
@@ -11,3 +16,26 @@ def customer_home_page(request):
         'text': 'داشبورد کارفرما'
     }
     return render(request, 'core/panel/pages/panel_home_page.html', context)
+
+
+class CustomerProjectList(CustomerAccessMixin, PermissionRequiredMixin, ListView):
+    def get_queryset(self):
+        qs = Project.objects.filter(
+            customers__in=[self.request.user], department__is_active=True, accessibility='only_customer'
+        )
+        return qs
+
+    template_name = 'core/panel/pages/project_list.html'
+    paginate_by = 6
+    context_object_name = 'projects'
+    permission_required = 'projects.view_project'
+
+
+class CustomerProjectDetail(CustomerAccessMixin, PermissionRequiredMixin, DetailView):
+    def get_object(self, queryset=None):
+        pk = self.kwargs.get('pk')
+        return Project.objects.get_project_for_customer(pk, self.request.user)
+
+    template_name = 'core/panel/pages/project_detail.html'
+    context_object_name = 'project'
+    permission_required = 'projects.view_project'
