e81d37a7eeb89f28bde4f5dd90ec682a29d54864
Ramachandran <ramachoon@gmail.com>
2022-07-09 18:38:38 +0430

    Add: CustomerWorkDayDetail view

--- src/projects/managers.py
+++ src/projects/managers.py
@@ -9,7 +9,6 @@ class ProjectManager(models.Manager):
         )
         return qs
 
-
     def get_project_for_customer(self, pk, user):
         qs = get_object_or_404(
             self, pk=pk, customers__in=[user], department__is_active=True, accessibility='only_customer'
@@ -28,6 +27,13 @@ class WorkDayManager(models.Manager):
     def get_work_day_for_staff(self, pk, user):
         qs = get_object_or_404(
             self, pk=pk, project__department__staff_users__in=[user], project__department__is_active=True
+        )
+        return qs
+
+    def get_work_day_for_customer(self, pk, user):
+        qs = get_object_or_404(
+            self, pk=pk, project__customers__in=[user], project__department__is_active=True,
+            project__accessibility='only_customer', accessibility='only_customer'
         )
         return qs
 
